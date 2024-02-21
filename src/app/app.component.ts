1380a5e8b4bf68d0004119d4b4581b2a75abb473
Michal Kaminski <MichalKaminski.KamienKaze@gmail.com>
2024-02-21 21:29:35 +0100

    enter animation finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 import WAVES from 'vanta/dist/vanta.waves.min.js';
+import { ArrowComponent } from './components/arrow/arrow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ArrowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -21,13 +23,13 @@ export class AppComponent {
       mouseControls: true,
       touchControls: true,
       gyroControls: false,
-      minHeight: 200.00,
-      minWidth: 200.00,
-      scale: 1.00,
-      scaleMobile: 1.00,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
       color: 0x0,
-      waveHeight: 11.00,
-      waveSpeed: 0.40,
-    })
+      waveHeight: 11.0,
+      waveSpeed: 0.4,
+    });
   }
 }
